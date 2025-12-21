2ac811631df5a2a0d22f92900b2256932f34b8ff
ytia001 <yiken36@gmail.com>
2025-12-21 22:04:08 +0800

    Add karma

---
+++ src/polyfills.ts
@@ -0,0 +1,7 @@
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
+ */
+
+// Zone JS is required by Angular itself
+import 'zone.js';  // Included with Angular CLI
