cd6c6fcdf17ef2343e8cdeeb1518d8780a8ada41
ytia001 <yiken36@gmail.com>
2025-12-22 10:15:45 +0800

    configure to add polyfills and update test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,5 @@
 import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
+
+getTestBed().initTestEnvironment(BrowserTestingModule, platformBrowserTesting());
